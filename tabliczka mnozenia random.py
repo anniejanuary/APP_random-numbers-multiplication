bb8a462670413757b769172974fde3c033752175
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-15 11:57:58 +0200

    Add files via upload

---
+++ tabliczka mnozenia random.py
@@ -0,0 +1,17 @@
+#program sprawadzajact znajomosc tabliczki mnozenia
+
+import random 
+
+a = random.randint(1,10) 
+b = random.randint(1,10)
+
+print("Podaj wynik iloczynu liczb", a , "i", b)
+multipl=int(input())
+
+if a*b == multipl_input:
+    print("Wynik prawidłowy")
+
+else:
+    print("Wynik nieprawidłowy")
+    
+
