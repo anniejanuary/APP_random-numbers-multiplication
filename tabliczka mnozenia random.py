d92ad9f795aa3739b2272fa452640d76ab825778
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-18 16:12:59 +0200

    Update tabliczka mnozenia random.py

--- tabliczka mnozenia random.py
+++ tabliczka mnozenia random.py
@@ -3,7 +3,7 @@ import random
 a = random.randint(1,10) 
 b = random.randint(1,10)
 
-print("The product of multiplying", a , "and", b "is: ")
+print("The product of multiplying", a , "and", b, "is: ")
 multipl=int(input())
 
 if a*b == multipl_input:
